a97619e9eaac19aa7fcf8de2548cb275e45f07b4
m4ln <runzmarlen@gmail.com>
2020-11-18 16:05:57 +0100

    added new script and small updates in image tiling

--- exportTiles.groovy
+++ exportTiles.groovy
@@ -1,41 +1,47 @@
 /**
  * Script to export image tiles (can be customized in various ways).
  */
+ 
 
-def saveDir = 'Z:\Marlen\datasets'
-def projectName = '\dieSchöneUndDasBiest_HE'
+//=============== SET VARIABLES ======================
+//==================================================== 
+// path to save data
+def saveDir = '/Z:/Marlen/datasets/beauty-and-beast/train_test_sets/test_FID/'
+// size of each tile, in pixels
+int tileSize = 512
+// downsample factor (1 = no downsampling)
+double downsample = 1
+// output resolution in calibrated units (e.g. µm if available)
+double requestedPixelSize = 0
+// overlap, in pixel units at the export resolution, (0 = no overlap)
+int overlapSize = tileSize/2
+//====================================================
+//====================================================
+
 def project = getProject()
-for (entry in project.getImageList()) {
+//for (entry in project.getImageList()) {
 
 // Get the current image (supports 'Run for project')
-def imageData = entry.readImageData()
-//def imageData = getCurrentImageData()
-
+//def imageData = entry.readImageData()
+def imageData = getCurrentImageData()
 // Define output path (here, relative to project)
 def name = GeneralTools.getNameWithoutExtension(imageData.getServer().getMetadata().getName())
-def pathOutput = buildFilePath(saveDir, projectName, name, 'qupathTiles')
+def pathOutput = buildFilePath(saveDir, name, ('size' + tileSize.toString() + '_overlap' + overlapSize.toString()))
 mkdirs(pathOutput)
+
 
 // Convert output resolution to a downsample factor
 double pixelSize = imageData.getServer().getPixelCalibration().getAveragedPixelSize()
-print('input pixelSize: ' + pixelSize)
 
-// Define output resolution in calibrated units (e.g. µm if available)
-double requestedPixelSize = 0
-print('output pixelSize: ' + requestedPixelSize)
-
-// downsample factor (1 = no downsampling)
-double downsample = 1
+// recompute downsampling if output resolution (requestedPixelSize) is set
 if (requestedPixelSize > 0)
     downsample = requestedPixelSize / pixelSize
+
+// print info
+print('input pixelSize: ' + pixelSize)
+print('output pixelSize: ' + requestedPixelSize)
 print('downsampling: ' + downsample)
-
-// Define size of each tile, in pixels
-int tileSize = 1024
 print('tileSize: ' + tileSize)
-
-// Define overlap, in pixel units at the export resolution, (0 = no overlap)
-int overlapSize = 0
 print('overlapSize: ' + overlapSize)
 
 // Create an exporter that requests corresponding tiles from the original & labelled image servers
@@ -47,5 +53,6 @@ new TileExporter(imageData)
     .overlap(overlapSize)              // Define overlap, in pixel units at the export resolution
     .writeTiles(pathOutput)   // Write tiles to the specified directory
 
-}
+//}
+
 print 'Done!'
